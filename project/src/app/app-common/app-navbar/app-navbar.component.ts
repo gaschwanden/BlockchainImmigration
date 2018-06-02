df393c39b09405468a27351f5a2917ed224f11dc
dagrawal <agrawal.dheeraj.7@gmail.com>
2018-06-02 20:24:02 +1000

    Styling

--- project/src/app/app-common/app-navbar/app-navbar.component.ts
+++ project/src/app/app-common/app-navbar/app-navbar.component.ts
@@ -1,16 +1,16 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './app-navbar.component.html',
-  styleUrls: ['./app-navbar.component.css']
+	selector: 'app-navbar',
+	templateUrl: './app-navbar.component.html',
+	styleUrls: ['./app-navbar.component.css']
 })
-export class AppNavbarComponent implements OnInit {
+export class AppNavbarComponent {
+	get ethAddress() {
+		return localStorage.getItem('ethAddress');
+	}
 
-  constructor() {
-  }
-
-  ngOnInit() {
-  }
-
+	get role() {
+		return localStorage.getItem('role');
+	}
 }
